dd6c83c4c81a2ee3780773fec2e65cf643f1a8d3
Marcelo Mena <marcelomena010@gmail.com>
2022-12-28 14:23:56 -0300

    feat: add cart sidebar no items message

--- components/common/CartSidebar/CartSidebar.tsx
+++ components/common/CartSidebar/CartSidebar.tsx
@@ -2,6 +2,16 @@ import Sidebar from '@components/Elements/Sidebar'
 import { SIDEBAR_ORIENTATION } from '@components/Elements/Sidebar/Sidebar'
 import { useLayout } from '../Layout/Context'
 
+const NoItemsMessage = () => (
+  <div className="text-center">
+    <span className="text-[15px] font-normal leading-snug">
+      Your cart is currently empty.
+      <br />
+      <a className="underline">Click here to continue shopping</a>.
+    </span>
+  </div>
+)
+
 const CartSidebar = () => {
   const { isCartSidebarOpen, closeCartSidebar } = useLayout()
 
@@ -11,7 +21,13 @@ const CartSidebar = () => {
       isOpen={isCartSidebarOpen}
       closeSidebar={closeCartSidebar}
     >
-      <div>cart items</div>
+      <div className="px-4 py-8">
+        <div className="pb-8 text-center font-light">
+          <h2 className="text-[22px]">Your Cart</h2>
+        </div>
+
+        <NoItemsMessage />
+      </div>
     </Sidebar>
   )
 }
