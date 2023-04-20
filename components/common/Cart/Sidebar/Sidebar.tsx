c80357f492c054181f536d61ec6dc29a67e7759c
Marcelo Mena <marcelomena011@gmail.com>
2023-04-20 15:35:53 -0300

    fix: lint errors

--- components/common/Cart/Sidebar/Sidebar.tsx
+++ components/common/Cart/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames'
 
 import Sidebar from '@components/elements/Sidebar'
-import { SIDEBAR_ORIENTATION } from '@components/elements/Sidebar'
+import { SIDEBAR_ORIENTATION } from '@components/elements/Sidebar/Sidebar'
 import SidebarProduct from './SidebarProduct'
 import SidebarFooter from './SidebarFooter'
 
