53a52bbada7d6c1cf70a0a516099d9d66dfd9c52
Marcelo Mena <marcelomena011@gmail.com>
2023-04-26 09:31:27 -0300

    fix: fixes

--- components/common/MenuSidebar/MenuSidebar.tsx
+++ components/common/MenuSidebar/MenuSidebar.tsx
@@ -1,5 +1,6 @@
 import Sidebar from '@components/Elements/Sidebar'
 import { useLayout } from '../Layout/Context'
+import Link from 'next/link'
 
 const MenuSidebar = () => {
   const { isMenuSidebarOpen, closeMenuSidebar } = useLayout()
@@ -16,23 +17,16 @@ const MenuSidebar = () => {
         <div>
           <ul>
             <li className="flex">
-              <a className="py-5 px-4 text-black text-[13px] font-light tracking-widest border-y-[1px] border-gray-200 w-full">
+              <Link
+                href="/"
+                className="py-5 px-4 text-black text-[13px] font-light tracking-widest border-y-[1px] border-gray-200 w-full"
+              >
                 HOME
-              </a>
+              </Link>
             </li>
             <li className="flex">
               <a className="py-5 px-4 text-black text-[13px] font-light tracking-widest border-b-[1px] border-gray-200 w-full">
                 SHOP
-              </a>
-            </li>
-            <li className="flex">
-              <a className="py-5 px-4 text-black text-[13px] font-light tracking-widest border-b-[1px] border-gray-200 w-full">
-                ABOUT
-              </a>
-            </li>
-            <li className="flex">
-              <a className="py-5 px-4 text-black text-[13px] font-light tracking-widest border-b-[1px] border-gray-200 w-full">
-                JOURNAL
               </a>
             </li>
           </ul>
